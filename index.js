abacb8f79dd8bcdc789734ff517b461868fae98b
Nurai <nooraselfeght@gmail.com>
2022-10-11 16:14:26 +0600

    updated file

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const executeMethod = (obj, keys, method, params) => {
   let targetKey = obj;
-  let keyArray = [];
-
-  if(keys.length <= 1){
-    keyArray.push(keys)
-  }else{
-    keyArray = keys.split('.');
-  }
+  let keyArray = keys.split('.');
 
   for(let i = 0; i < keyArray.length; i++){
     if(!targetKey.hasOwnProperty(keyArray[i])){return null}
@@ -14,14 +8,7 @@ const executeMethod = (obj, keys, method, params) => {
     targetKey = targetKey[keyArray[i]]
   }
 
-
-  let paramsList = '';
-
-  for(let  i = 0; i < params.length; i++){
-    paramsList += ', `' + params[i] + '`'
-  };
-
-  return(eval(`${JSON.stringify(targetKey)}.${method}(${paramsList.substring(1)})`))
+  return targetKey[`${method}`](...params)
 };
 
 module.exports = executeMethod;
